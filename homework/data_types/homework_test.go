2a73de1bedf4a68a3078d3b33f89f1e8b1e09b23
AlexWinS <anenahov@digital-spirit.ru>
2025-08-01 01:53:52 +0300

    task 1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
-
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+type MyInt interface {
+	~uint32 | ~uint16 | ~uint64
 }
 
-func TestСonversion(t *testing.T) {
+func ToLittleEndian[T MyInt](number T) T {
+	startP := unsafe.Pointer(&number)
+	size := int(unsafe.Sizeof(number))
+	endP := unsafe.Add(startP, size-1)
+	for i := 0; i < size/2; i++ {
+		firsByte := (*byte)(unsafe.Add(startP, i))
+		lastByte := (*byte)(unsafe.Add(endP, -i))
+		*firsByte, *lastByte = *lastByte, *firsByte
+	}
+	return number
+}
+
+func TestConversion(t *testing.T) {
 	tests := map[string]struct {
 		number uint32
 		result uint32
@@ -40,6 +52,40 @@ func TestСonversion(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+
+	uint16Tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"uint16 #1": {number: 0x0000, result: 0x0000},
+		"uint16 #2": {number: 0xFFFF, result: 0xFFFF},
+		"uint16 #3": {number: 0x1234, result: 0x3412},
+		"uint16 #4": {number: 0x00FF, result: 0xFF00},
+	}
+
+	for name, test := range uint16Tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+
+	uint64Tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"uint64 #1": {number: 0x0000000000000000, result: 0x0000000000000000},
+		"uint64 #2": {number: 0xFFFFFFFFFFFFFFFF, result: 0xFFFFFFFFFFFFFFFF},
+		"uint64 #3": {number: 0x0102030405060708, result: 0x0807060504030201},
+		"uint64 #4": {number: 0x00000000FFFFFFFF, result: 0xFFFFFFFF00000000},
+	}
+
+	for name, test := range uint64Tests {
 		t.Run(name, func(t *testing.T) {
 			result := ToLittleEndian(test.number)
 			assert.Equal(t, test.result, result)
