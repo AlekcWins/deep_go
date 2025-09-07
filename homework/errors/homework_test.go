75e8b9956d18cd38ffa50986a5fb9b458425d801
AlexWinS <anenahov@digital-spirit.ru>
2025-09-07 19:13:49 +0300

    task 8: Multi error

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"slices"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +13,43 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errors []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	if e == nil || len(e.errors) == 0 {
+		return ""
+	}
+
+	b := strings.Builder{}
+	b.WriteString(fmt.Sprintf("%d errors occurred:\n", len(e.errors)))
+
+	for _, err := range e.errors {
+		b.WriteString(fmt.Sprintf("\t* %s", err.Error()))
+	}
+
+	b.WriteString("\n")
+	return b.String()
 }
 
-func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+func Append(err error, errs ...error) (res *MultiError) {
+	errs = slices.DeleteFunc(errs, func(e error) bool {
+		return e == nil
+	})
+
+	if err == nil {
+		return &MultiError{errors: errs}
+	}
+
+	multiError, ok := err.(*MultiError)
+	if !ok {
+		return &MultiError{
+			errors: append([]error{err}, errs...),
+		}
+	}
+	multiError.errors = append(multiError.errors, errs...)
+
+	return multiError
 }
 
 func TestMultiError(t *testing.T) {
@@ -28,6 +57,20 @@ func TestMultiError(t *testing.T) {
 	err = Append(err, errors.New("error 1"))
 	err = Append(err, errors.New("error 2"))
 
-	expectedMessage := "2 errors occured:\n\t* error 1\t* error 2\n"
+	expectedMessage := "2 errors occurred:\n\t* error 1\t* error 2\n"
 	assert.EqualError(t, err, expectedMessage)
+}
+
+func TestMultiError_AppendNilErrors(t *testing.T) {
+	err := Append(nil, errors.New("real error"), nil)
+	expected := "1 errors occurred:\n\t* real error\n"
+	assert.EqualError(t, err, expected)
+}
+
+func TestMultiError_AppendToRegularError(t *testing.T) {
+	base := errors.New("base error")
+	err := Append(base, errors.New("extra 1"), errors.New("extra 2"))
+
+	expected := "3 errors occurred:\n\t* base error\t* extra 1\t* extra 2\n"
+	assert.EqualError(t, err, expected)
 }
